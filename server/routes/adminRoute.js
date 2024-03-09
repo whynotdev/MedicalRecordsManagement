0a15931c2e55618b184aa764553254264429e9cf
Arif <115531149+whynotdev@users.noreply.github.com>
2024-03-10 00:00:36 +0530

    Admin

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -46,4 +46,42 @@ router.get("/get-all-users", authMiddleware, async (req, res) => {
 });
 
 
+//change status of pending doctor
+router.post(
+  "/change-doctor-account-status",
+  authMiddleware,
+  async (req, res) => {
+    try {
+      const { doctorId, status } = req.body;
+      const doctor = await Doctor.findByIdAndUpdate(doctorId, {
+        status,
+      });
+
+      const user = await User.findOne({ _id: doctor.userId });
+      const unseenNotifications = user.unseenNotifications;
+      unseenNotifications.push({
+        type: "new-doctor-request-changed",
+        message: `Your doctor account has been ${status}`,
+        onClickPath: "/notifications",
+      });
+      user.isDoctor = status === "approved" ? true : false;
+      await user.save();
+
+      res.status(200).send({
+        message: "Doctor status updated successfully",
+        success: true,
+        data: doctor,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        message: "Error applying doctor account",
+        success: false,
+        error,
+      });
+    }
+  }
+);
+
+
 module.exports = router;
